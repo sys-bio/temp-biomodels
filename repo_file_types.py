4a1be06870c7232a8794c367508be8e7621b046d
Andrew Freiburger <Andrewefreiburger@gmail.com>
2022-03-13 14:53:01 -0400

    notating corrupted scripts in the repo

---
+++ repo_file_types.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  7 00:37:38 2022
+
+@author: Andrew Freiburger
+"""
+from glob import glob
+import os, re
+
+#file_types = set()
+#for file in glob(os.path.join('final', '*/*')):
+#    extension = re.sub('(.+\.)','',file)
+#    file_types.add(extension)
+#    
+#print(file_types)
+
+for file in glob(os.path.join('final', '*/*.ipynb'), recursive = True):
+    print(file)
