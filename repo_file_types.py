5b58ced183486d872ea035acc83ff8d72a3f63f2
Andrew Freiburger <Andrewefreiburger@gmail.com>
2022-03-13 14:54:10 -0400

    uncomment repo_file_types

--- repo_file_types.py
+++ repo_file_types.py
@@ -7,12 +7,12 @@ Created on Mon Mar  7 00:37:38 2022
 from glob import glob
 import os, re
 
-#file_types = set()
-#for file in glob(os.path.join('final', '*/*')):
-#    extension = re.sub('(.+\.)','',file)
-#    file_types.add(extension)
-#    
-#print(file_types)
+file_types = set()
+for file in glob(os.path.join('final', '*/*')):
+    extension = re.sub('(.+\.)','',file)
+    file_types.add(extension)
+    
+print(file_types)
 
 for file in glob(os.path.join('final', '*/*.ipynb'), recursive = True):
     print(file)
