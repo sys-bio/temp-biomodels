b1186c946d6e6eebc2edcd6888a99eeff411df4c
Jonathan Karr <jonrkarr@gmail.com>
2022-03-29 23:11:00 -0400

    chore: cleaned up rename_xpp_to_ode.py

--- rename_xpp_to_ode.py
+++ rename_xpp_to_ode.py
@@ -1,14 +1,13 @@
-
 # Files that end in '.xpp' should instead end with '.ode'
 from os import rename
 
+
 def run(xpp_filenames):
-    """ Rename xpp files
+    """ Rename XPP files
 
     Args:
-        xpp_filenames(:obj:'list'): list of '.xpp' files to rename.
-
+        xpp_filenames (:obj:`list` of :obj:`str`): list of ``.xpp`` files to rename
     """
-    for xppname in xpp_filenames:
-        odename = xppname.replace(".xpp", ".ode")
-        rename(xppname, odename)
+    for xpp_filename in xpp_filenames:
+        ode_filename = xpp_filename.replace(".xpp", ".ode")
+        rename(xpp_filename, ode_filename)
