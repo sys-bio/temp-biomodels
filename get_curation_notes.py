f1d340558ac9a3625bbf1e391af4838cbded1158
Andrew Freiburger <Andrewefreiburger@gmail.com>
2022-03-31 11:56:49 -0400

    curation image update

--- get_curation_notes.py
+++ get_curation_notes.py
@@ -10,7 +10,7 @@ import warnings
 
 def acquire_note(directory, div):
     if div.find('div').text is not None:
-        with open(os.path.join(directory, f'{model_id}_curation_notes.txt'), 'w', encoding='utf-8') as out:
+        with open(os.path.join(directory, f'curation_notes.txt'), 'w', encoding='utf-8') as out:
             out.write(div.find('div').text.strip())
     else:
         warnings.warn(f'Curation note could not be parsed for {model_id}', UserWarning)
@@ -21,19 +21,21 @@ def acquire_image(directory, div):
         for img_file in div.find_all('img'):
             img_format = img_file['src'].partition('/')[2].partition(';')[0]
             img_data = b64decode(img_file['src'].partition(',')[2])
-            img_filename = os.path.join(directory, f'{model_id}_curation_image.{img_format}')
+            img_filename = os.path.join(directory, f'curation_image.{img_format}')
             with open(img_filename, 'wb') as out:
                 out.write(img_data)
             correct_img_format = imghdr.what(img_filename)
             if correct_img_format != img_format:
-                os.rename(img_filename, os.path.join(directory, f'{model_id}_curation_image.{correct_img_format}'))
+                os.rename(img_filename, os.path.join(directory, f'curation_image.{correct_img_format}'))
     else:
         warnings.warn(f'Curation image could not be found for {model_id}', UserWarning)
 
 
-for model_path in glob(os.path.join('..', 'temp-biomodels', 'manual-fixes', '*')):
+for model_path in glob(os.path.join('manual-fixes', '*')):
     # acquire the raw HTML
     model_id = os.path.basename(model_path)
+    if model_id != 'BIOMD0000000901':
+        continue
     url = f'https://www.ebi.ac.uk/biomodels/{model_id}#Curation'
     response = requests.get(url, headers={'accept': 'application/html'})
     soup = BeautifulSoup(response.text, 'lxml')
