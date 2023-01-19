f42378fb724110d5334f88dd2b64f8df1d2ccee0
Lucian Smith <lpsmith@uw.edu>
2023-01-19 12:45:39 -0800

    Fix bug where there were two manifest files.

--- create_omex.py
+++ create_omex.py
@@ -7,11 +7,14 @@ import glob
 combine_writer = CombineArchiveWriter()
 
 
-def process(project_id, project_path, omex_dir, write_omex):
+def process(project_id, project_path, omex_dir, write_omex, always_create_manifest=True):
     manifest_filename = os.path.join(project_path, 'manifest.xml')
     sedml_filenames = glob.glob(os.path.join(project_path, '**', '*.sedml'), recursive=True)
     sedml_locations = [os.path.relpath(path, project_path) for path in sedml_filenames]
     archive = build_combine_archive(project_path, sedml_locations)
-    combine_writer.write_manifest(archive.contents, manifest_filename)
+    if always_create_manifest and os.path.exists(manifest_filename):
+        os.remove(manifest_filename)
+    if not os.path.exists(manifest_filename):
+        combine_writer.write_manifest(archive.contents, manifest_filename)
     if write_omex:
         combine_writer.run(archive, project_path, omex_dir + "/" + project_id + ".omex")
