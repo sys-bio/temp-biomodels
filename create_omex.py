76a50397f9870ac1fe525df27468db395c11e51c
Lucian Smith <lpsmith@uw.edu>
2023-01-19 13:58:36 -0800

    Have to delete existing manifest file before building archive.

--- create_omex.py
+++ create_omex.py
@@ -7,14 +7,15 @@ import glob
 combine_writer = CombineArchiveWriter()
 
 
-def process(project_id, project_path, omex_dir, write_omex, always_create_manifest=True):
+def process(project_id, project_path, omex_dir, write_omex):
     manifest_filename = os.path.join(project_path, 'manifest.xml')
     sedml_filenames = glob.glob(os.path.join(project_path, '**', '*.sedml'), recursive=True)
     sedml_locations = [os.path.relpath(path, project_path) for path in sedml_filenames]
-    archive = build_combine_archive(project_path, sedml_locations)
-    if always_create_manifest and os.path.exists(manifest_filename):
+    if os.path.exists(manifest_filename):
+        #Have to do this, otherwise build_combine_archive creates a new one!
         os.remove(manifest_filename)
-    if not os.path.exists(manifest_filename):
-        combine_writer.write_manifest(archive.contents, manifest_filename)
+    archive = build_combine_archive(project_path, sedml_locations)
+    #Write the new manifest.xml file:
+    combine_writer.write_manifest(archive.contents, manifest_filename)
     if write_omex:
         combine_writer.run(archive, project_path, omex_dir + "/" + project_id + ".omex")
